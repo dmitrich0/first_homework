904fa3028bd93ca17f321eb26fbe6bb549b17953
Nikita Ivanov <ivanovvnikita@mail.ru>
2023-05-20 17:30:07 +0500

    EVENT-FIX

---
+++ src/main/java/com/example/first_homework/EventService.java
@@ -0,0 +1,47 @@
+package com.example.first_homework;
+
+import com.example.first_homework.event.AsyncEvent;
+import com.example.first_homework.event.CommonEvent;
+import com.example.first_homework.event.TransactionalEvent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class EventService {
+    @Autowired
+    private ApplicationEventPublisher applicationEventPublisher;
+
+    public String generateCommonEvent() {
+        var event = new CommonEvent();
+        applicationEventPublisher.publishEvent(event);
+        return "Common event";
+    }
+
+    public String generateAsyncEvent() {
+        var event = new AsyncEvent();
+        applicationEventPublisher.publishEvent(event);
+        return "Async event";
+    }
+
+    @Transactional
+    public String generateTransactionalEvent() throws Exception {
+        var event = new TransactionalEvent();
+        applicationEventPublisher.publishEvent(event);
+        return "Transactional event";
+    }
+
+    @Transactional
+    public String generateTransactionalErrorEvent() {
+        try {
+            var event = new TransactionalEvent();
+            applicationEventPublisher.publishEvent(event);
+            event.makeError();
+            return "Transactional event";
+        } catch (Exception ex) {
+            return "Transactional event with error";
+        }
+    }
+}
