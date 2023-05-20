48bc17a28e63a3960c166dc3d9f6626290db7250
Nikita Ivanov <ivanovvnikita@mail.ru>
2023-05-20 17:51:06 +0500

    EVENT-FIX

--- src/main/java/com/example/first_homework/aspect/ApiAspect.java
+++ src/main/java/com/example/first_homework/aspect/ApiAspect.java
@@ -9,18 +9,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Slf4j
 @Component
 @Aspect
 public class ApiAspect {
-    private Integer counter = 0;
+    private final Map<String, Integer> apiCalls = new HashMap<>();
     @Autowired
-    ApiConfiguration apiConfiguration;
+    private ApiConfiguration apiConfiguration;
 
     @Around("@annotation(ApiAudit)")
     public ResponseEntity<String> auditApi(ProceedingJoinPoint joinPoint) throws Throwable {
-        counter += 1;
-        if (counter > this.apiConfiguration.getMaxApiCalls()) {
+        var key = joinPoint.toString();
+        if (!apiCalls.containsKey(key)){
+            apiCalls.put(key, 1);
+        } else {
+            apiCalls.put(key, apiCalls.get(key) + 1);
+        }
+        if (apiCalls.get(key) > this.apiConfiguration.getMaxApiCalls()) {
             return new ResponseEntity<>("Too many requests! Max requests count is %s".formatted(this.apiConfiguration
                     .getMaxApiCalls()), HttpStatus.TOO_MANY_REQUESTS);
         } else {
